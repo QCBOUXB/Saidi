b6187a85957aaa9005ad2908d973dc72374c765e
QCBOUXB <102544818+QCBOUXB@users.noreply.github.com>
2022-12-05 17:08:07 +0200

    Add files via upload

--- handlers/gpcmd.py
+++ handlers/gpcmd.py
@@ -790,10 +790,10 @@ __italic__
           listTag = ""
           i = 1
           for u in tagall:
-            if u.user.first_name:
-              listTag = listTag+"\n"+str(i)+" » [{}](tg://user?id={})".format(u.user.first_name,u.user.id)
+            if u.user.username:
+              listTag = listTag+"\n"+str(i)+" - [@{}]".format(u.user.username)
             else:
-              listTag = listTag+"\n"+str(i)+" » [{}](tg://user?id={})".format(u.user.first_name,u.user.id)
+              listTag = listTag+"\n"+str(i)+" - [{}](tg://user?id={})".format(u.user.first_name,u.user.id)
             i += 1 
           sendM("NO",listTag,message)
           
